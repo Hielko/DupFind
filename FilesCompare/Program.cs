13825e302270190efb8a4be7c55f4c40ebad6501
Hielko Wiegers <hielko.wiegers@perplex.nl>
2023-06-27 14:07:46 +0200

    Update

--- FilesCompare/Program.cs
+++ FilesCompare/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using FilesCompare;
 
+var x = args[0];
+
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+
 Console.WriteLine("Hello, World!");
-//new Files(new string[] { "c:\\test\\" } );
+var y = new CompareBySize().Compare(new string[] { $"{Directory.GetCurrentDirectory()}" } );
+
+foreach (var n in y)
+{
+    var si = string.Join(", ", n.Item2.Select(x => x.FullName));
+    Console.WriteLine($"{n.Item1} is in {si}");
+}
